68a3110733c6dc0bcd571d13fe26dc5b0f1373a4
Sara Harvey <saraharveynyc@gmail.com>
2019-08-02 15:24:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,19 @@
+Learn more
 class App < Sinatra::Base
-  get '/' do
-   erb :index
-  end
 
-  get '/hello' do
-   erb :hello
-  end
+	get '/' do
+		erb :index
+	end
 
-  get '/goodbye' do
-   erb :goodbye
-  end
+	get '/hello' do 
+		erb :hello 
+	end
 
-  get '/date' do
-   erb :date
-  end
+	get '/goodbye' do 
+		erb :goodbye
+	end
 
+	get '/date' do 
+		erb :date
+	end
 end
